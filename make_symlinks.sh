0bf8314cdd1115c60536ea427f3b3baebbf26899
Dave Bolton <david@davidbolton.net>
2009-01-09 17:35:50 +1100

    added script to make symlinks

---
+++ make_symlinks.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+ln -s /Users/davebolton/home/vimrc .vimrc
+ln -s /Users/davebolton/home/rdebugrc .rdebugrc
+ln -s /Users/davebolton/home/gvimrc .gvimrc
+ln -s /Users/davebolton/home/bash_aliases .bash_aliases
+ln -s /Users/davebolton/home/bash_profile .bash_profile
