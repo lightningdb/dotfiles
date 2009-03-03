f5a4c5fdaa78135066e7de44fa2144471f66a964
Dave Bolton <david@davidbolton.net>
2009-03-04 10:48:38 +1100

    updated vim-rails and added gemrc

--- make_symlinks.sh
+++ make_symlinks.sh
@@ -2,5 +2,6 @@
 ln -s /Users/davebolton/home/vimrc .vimrc
 ln -s /Users/davebolton/home/rdebugrc .rdebugrc
 ln -s /Users/davebolton/home/gvimrc .gvimrc
+ln -s /Users/davebolton/home/gemrc .gemrc
 ln -s /Users/davebolton/home/bash_aliases .bash_aliases
 ln -s /Users/davebolton/home/bash_profile .bash_profile
