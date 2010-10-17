c4472205a6cc3b71f70e8e7102644579df2a09a6
David Bolton <david@davidbolton.net>
2010-10-18 10:39:57 +1100

    updated symlink creation script for cross platformity

--- make_symlinks.sh
+++ make_symlinks.sh
@@ -1,7 +1,8 @@
 #!/bin/zsh
-ln -s /home/davidbolton/home/vimrc .vimrc
-ln -s /home/davidbolton/home/rdebugrc .rdebugrc
-ln -s /home/davidbolton/home/gvimrc .gvimrc
-ln -s /home/davidbolton/home/gemrc .gemrc
-ln -s /home/davidbolton/home/bash_aliases .bash_aliases
-ln -s /home/davidbolton/home/bash_profile .bash_profile
+ln -s ~/home/vimrc ~/.vimrc
+ln -s ~/home/rdebugrc ~/.rdebugrc
+ln -s ~/home/gvimrc ~/.gvimrc
+ln -s ~/home/gemrc ~/.gemrc
+ln -s ~/home/bash_aliases ~/.bash_aliases
+ln -s ~/home/bash_profile ~/.bash_profile
+ln -s ~/home/screenrc ~/.screenrc
