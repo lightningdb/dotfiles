971d68b2271dd77e2b2036f4495f14a7e02772c9
David Bolton <david@davidbolton.net>
2011-02-21 16:19:27 +1100

    added taskwarrior config

--- make_symlinks.sh
+++ make_symlinks.sh
@@ -7,3 +7,4 @@ ln -s ~/home/bash_aliases ~/.bash_aliases
 ln -s ~/home/bash_profile ~/.bash_profile
 ln -s ~/home/screenrc ~/.screenrc
 ln -s ~/home/tmux.conf ~/.tmux.conf
+ln -s ~/home/taskrc ~/.taskrc
