efe2080794934957a5a26157bd51d7faf9659229
David Bolton <dave@davebolton.net>
2011-05-06 08:57:50 +1000

    updated symlinks

--- make_symlinks.sh
+++ make_symlinks.sh
@@ -4,3 +4,5 @@ ln -s ~/home/rdebugrc ~/.rdebugrc
 ln -s ~/home/gvimrc ~/.gvimrc
 ln -s ~/home/tmux.conf ~/.tmux.conf
 ln -s ~/home/taskrc ~/.taskrc
+ln -s ~/home/.task ~/.task
+ln -s ~/home/vimwiki ~/vimwiki
