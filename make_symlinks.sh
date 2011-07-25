0aa402cc7529c399f32b0ed002d6bc018b2173ce
Dave Bolton <david@davidbolton.net>
2011-07-26 03:24:41 +1000

    updated symlinks script to include vim folder

--- make_symlinks.sh
+++ make_symlinks.sh
@@ -2,6 +2,7 @@
 ln -s ~/home/ackrc ~/.ackrc
 ln -s ~/home/vimrc ~/.vimrc
 ln -s ~/home/gvimrc ~/.gvimrc
+ln -s ~/home/vim ~/.vim
 ln -s ~/home/rdebugrc ~/.rdebugrc
 ln -s ~/home/tmux.conf ~/.tmux.conf
 ln -s ~/home/taskrc ~/.taskrc
