4827ca2daaa5626dbb9a62d4ffc47d177da48c62
Bolton <boltond@NDMML1005570.local>
2013-04-09 12:06:42 +1000

    updated symlink creation

--- make_symlinks.sh
+++ make_symlinks.sh
@@ -1,19 +1,13 @@
-#!/bin/zsh
-files="
-ackrc
-gvimrc
-rdebugrc
-riplrc
-taskrc
-tmux.conf
-vimrc
-"
+#!/usr/local/bin/zsh
+files=( ackrc gvimrc rdebugrc riplrc taskrc tmux.conf vimrc )
 
 for file in $files
 do
-  rm "~/.${file}" && ln -s ~/dotfiles/${file} ~/.${file}
+  rm "~/.${file}"
+  ln -s ~/dotfiles/${file} ~/.${file}
 done
 
 # Left the edgecases alone
-rm ~/.task && ln -s ~/Dropbox/.task ~/.task
+rm ~/.task
+ln -s ~/Dropbox/.task ~/.task
 ln -s ~/Dropbox/vimwiki ~/vimwiki
